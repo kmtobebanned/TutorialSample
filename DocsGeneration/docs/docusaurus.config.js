ae4e9f8120894e8d2930cbf7cf5488dea208911e
kemalmsi <kemal.maulidiasani@accelbyte.net>
2023-01-12 11:44:31 +0700

    edit docu config

--- DocsGeneration/docs/docusaurus.config.js
+++ DocsGeneration/docs/docusaurus.config.js
@@ -14,16 +14,16 @@ let snippets = JSON.parse(rawdata);
 const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
+  url: 'https://kmtobebanned.github.io',
+  baseUrl: '/TutorialSample/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'kmtobebanned', // Usually your GitHub org/user name.
+  projectName: 'TutorialSample', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
